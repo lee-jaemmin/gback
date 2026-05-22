05d0383c6cdf05ab1a7fe8c664ce6a2b275af24c
lee-jaemmin <dlwoals8546@gmail.com>
2026-05-22 13:02:38 +0900

    [FEAT] schemas.py 기초 5개 스키마 작성: Table, User, Company, Item, TablePurchase

--- schemas.py
+++ schemas.py
@@ -1,32 +1,53 @@
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
-from typing import List
 
 # 플러터 앱과 주고받을 테이블 데이터의 기본 형태
+# API 명세서
+# Base: 여러 스키마서 공통으로 반복되는 필드. 즉, 일반 속성
 class TableBase(BaseModel):
     tablename: str
     section: str
-    status: Optional[str] = "available"
-    customer: Optional[str] = ""
-    phonenumber: Optional[str] = ""
-    staff: Optional[str] = ""
-    bottle: Optional[str] = ""
-    persons: Optional[int] = 0
-    remark: Optional[str] = ""
-    reservation_time: Optional[str] = None
+    status: str = "available"
+    customer: str = ""
+    phonenumber: str = ""
+    persons: int = 0
+    remark: str = ""
+    total_price: int = 0
+    registered_at: Optional[datetime] = None
+    ismaster: bool = False
+    mastertablenumber: Optional[str] = None
 
-# [Create] 관리자가 새 테이블을 만들 때 앱에서 서버로 보내야 하는 필수 데이터
+# [Create] 새 Row를 만들 때 클라이언트가 반드시/선택적으로 보내야하는 필드
+# 식별값/소속값
 class TableCreate(TableBase):
-    tid: str
+    id: str
     company_id: str
+    user_id: Optional[str] = None
+    group_id: Optional[str] = None
+
+# [Update] 수정 시.
+class TableUpdate(BaseModel):
+    tablename: Optional[str] = None
+    section: Optional[str] = None
+    status: Optional[str] = None
+    customer: Optional[str] = None
+    phonenumber: Optional[str] = None
+    persons: Optional[int] = None
+    remark: Optional[str] = None
+    total_price: Optional[int] = None
+    registered_at: Optional[datetime] = None
+    user_id: Optional[str] = None
+    group_id: Optional[str] = None
+    ismaster: Optional[bool] = None
+    mastertablenumber: Optional[str] = None 
 
 # [Response] 서버가 앱으로 데이터를 보낼 때의 규격
 class TableResponse(TableBase):
-    tid: str
+    id: str
     company_id: str
+    user_id: Optional[str] = None
     group_id: Optional[str] = None
-    is_master: Optional[bool] = False
     created_at: datetime
     updated_at: datetime
 
@@ -34,31 +55,49 @@ class TableResponse(TableBase):
     class Config:
         from_attributes = True
 
-# [Register] 스태프가 손님 앉히고 정보 입력
-class TableRegister(BaseModel):
-    customer: str
-    phonenumber: str
-    bottle: str
-    persons: int
-    remark: str
-    staff: str
-    status: str = "inuse"
+class CompanyBase(BaseModel):
+    name: str
+    addr: str
+
+class CompanyCreate(CompanyBase):
+    id: str
+
+class CompanyUpdate(BaseModel):
+    name: Optional[str] = None
+    addr: Optional[str] = None
+    id: Optional[str] = None
+
+class CompanyResponse(CompanyBase):
+    id: str
+    created_at: datetime
     updated_at: datetime
+    
+    class Config:
+        from_attributes = True
 
-# [Reserve] 예약
-class TabelReservation(BaseModel):
-    reservation_time: Optional[str] = None
+class UserBase(BaseModel):
+    username: str
+    email: str
+    role: str = 'user'
+    fcmtoken: str
+    tablecardfields : JSON 
 
-# [Move] 이동
-class TableMode(BaseModel):
-    from_tid: str
-    to_tid: str
+class UserCreate(UserBase):
+    id: str
+    company_id: str
 
-# [Join] 합석
-class TableJoin(BaseModel):
-    master_tid: str
-    slave_tids: List[str]
+class UserUpdate(BaseModel):
+    id: Optional[str] = None
+    company_id: Optional[str] = None
+    username: Optional[str] = None
+    email: Optional[str] = None
+    role: Optional[str] = None
+    fcmtoken: Optional[str] = None
+    tablecardfields: Optional[JSON] = None
 
-# [Unjoin] 합석 해제
-class TableJoin(BaseModel):
-    group_id: str
+class UserResponse(UserBase):
+    id: str
+    company_id: str
+    created_at: datetime
+    class Config:
+        from_attributes = True
