1d7dfe370df7eb48ad97bd4ef836d5b6299699d9
lee-jaemmin <dlwoals8546@gmail.com>
2026-06-17 10:49:06 +0900

    main: read_res_purchases_by_reservation 추가

--- main.py
+++ main.py
@@ -515,6 +515,16 @@ def read_res_purchases_by_table(
         raise HTTPException(status_code=404, detail="Table not found")
     return crud.get_res_purchases_by_table(db, table_id)
 
+@app.get("/reservations/{reservation_id}/res-purchases", response_model=list[schemas.ReservationPurchaseResponse])
+def read_res_purchases_by_reservation(
+    reservation_id: int,
+    db: Session = Depends(get_db)
+):
+    db_reservation = crud.get_table(db, reservation_id)
+    if db_reservation is None:
+        raise HTTPException(status_code=404, detail="Reservation not found")
+    return crud.get_res_purchases_by_reservation(db, reservation_id)
+
  
 def update_res_purchase(
     res_purchase_id: int,
