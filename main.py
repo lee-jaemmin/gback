1d1f8e962ad054d99d444a4844eb779c3cc44c99
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-18 09:23:07 +0900

    [add] 예약 되어있는 테이블 하나라도 있으면 일괄 마감 설정 불가

--- main.py
+++ main.py
@@ -596,6 +596,8 @@ def set_tables_bid_end_at_all(
     if not is_staff_company:
         raise HTTPException(status_code=403, detail="Permission Denied")
     db_tables = crud.get_tables_by_company(db, db_company.id)
+    if any(table.is_reserved for table in db_tables):
+        raise HTTPException(status_code=409, detail="Table already reserved")
     for table in db_tables:
         table.bid_end_at = bid_option.bid_end_at
     db.commit()
