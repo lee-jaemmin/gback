b484262b432997ec2bea06b67c021de2087ba112
lee-jaemmin <dlwoals8546@gmail.com>
2026-09-22 13:59:11 +0900

    [del] CORS 정리

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ cors_allowed_origins = [
     origin.strip()
     for origin in os.getenv(
         "CORS_ALLOWED_ORIGINS",
-        "http://localhost:3000,http://127.0.0.1:3000, http://localhost:38337",
+        "https://tablebid.kr", "https://tablebid-chi.vercel.app",
     ).split(",")
     if origin.strip()
 ]
